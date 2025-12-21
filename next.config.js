1790d06ca83ff05d289478475ed2d84afa333925
NMMonteiro <nunommonteiro@msn.com>
2025-12-21 17:03:23 +0000

    Revert to Next.js 15 and Middleware standard

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactCompiler: true,
-    // Removed deprecated eslint and typescript config blocks
-};
-
-module.exports = nextConfig;
