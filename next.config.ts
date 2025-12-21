9212ae0d09c4f8c728db3b7c186cf43fb03cf033
NMMonteiro <nunommonteiro@msn.com>
2025-12-21 17:05:46 +0000

    Strip all experimental options from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    experimental: {
-        reactCompiler: true,
-    },
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    typescript: {
-        ignoreBuildErrors: true,
-    },
+    /* config options here */
 };
 
 export default nextConfig;
