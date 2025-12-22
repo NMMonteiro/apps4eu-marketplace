2baab78e260ef0dd397d996dd7f78bb33864e245
NMMonteiro <nunommonteiro@msn.com>
2025-12-22 13:43:59 +0000

    fix: remove accidental import in confirm route

--- src/app/auth/confirm/route.ts
+++ src/app/auth/confirm/route.ts
@@ -1,5 +1,4 @@
 import { type EmailOtpType } from '@supabase/supabase-js'
-import { next } from 'process'
 import { NextResponse } from 'next/server'
 import { createClient } from '@/lib/supabase/server'
 
